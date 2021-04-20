9914cba7bb0a271403b44a7ed465a62b2113f08d
oskar566 <oschr20@student.sdu.dk>
2021-04-20 15:09:20 +0200

    test

--- src/Presentation/Main.java
+++ src/Presentation/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        //hej mit navn er oskar
     }
     public static void main(String[] args) {
         launch(args);
