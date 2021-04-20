eb1dfc40b5ae9cfe44cd960d425bfc7360d47808
oskar566 <oschr20@student.sdu.dk>
2021-04-20 15:33:11 +0200

    Update Participant.java

--- src/Domain/Participant.java
+++ src/Domain/Participant.java
@@ -2,4 +2,37 @@ package Domain;
 
 public class Participant
 {
+    private int id;
+    private String name;
+    private String occupation;
+
+    public Participant(int id, String name, String occupation) {
+        this.id = id;
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
 }
