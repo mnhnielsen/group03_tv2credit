5f41f473f4d9eb94e90f8c588070b15abc6e623f
oskar566 <oschr20@student.sdu.dk>
2021-04-21 11:19:39 +0200

    Added Production and Participant Constructor + Setter and getter

--- src/Domain/Participant.java
+++ src/Domain/Participant.java
@@ -4,35 +4,31 @@ public class Participant
 {
     private int id;
     private String name;
-    private String occupation;
 
-    public Participant(int id, String name, String occupation) {
+    public Participant (int id, String name)
+    {
         this.id = id;
         this.name = name;
-        this.occupation = occupation;
     }
 
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public String getOccupation() {
-        return occupation;
-    }
-
-    public void setOccupation(String occupation) {
-        this.occupation = occupation;
-    }
 }
