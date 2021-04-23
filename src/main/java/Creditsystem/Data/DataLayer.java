be43bfb4c728c8514f52486f06e5a52e918b404d
Frederik Nielsen <frederik@icsharp.io>
2021-04-23 12:58:04 +0200

    Added readFile() in dataLayer
    yeeeehaaaaaw

--- src/main/java/Creditsystem/Data/DataLayer.java
+++ src/main/java/Creditsystem/Data/DataLayer.java
@@ -1,6 +1,31 @@
 package Creditsystem.Data;
 
+import Creditsystem.Domain.ProducerController;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DataLayer
 {
-    
+    public static ArrayList<String> readFile(String fileName) {
+        String content = "";
+        ArrayList<String> stringContent = new ArrayList<>();
+        try {
+            File file = new File(fileName);
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine())
+            {
+                content=reader.nextLine();
+                stringContent.add(content+"\n");
+            }
+            reader.close();
+            return stringContent;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringContent;
+    }
 }
