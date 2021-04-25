7abce40eafd7c0300427afe77da957de0e583d7c
oskar566 <oschr20@student.sdu.dk>
2021-04-25 13:40:42 +0200

    Changes

--- src/main/java/Creditsystem/Domain/InformationController.java
+++ src/main/java/Creditsystem/Domain/InformationController.java
@@ -2,19 +2,45 @@ package Creditsystem.Domain;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class InformationController
 {
     //TODO fix this!
     public TextArea creditInfo_txtArea;
-    public Label title;
+    public Label lblTitle;
+    public Label lblReleaseYear;
+    public TextArea txtDescription;
+    public TableView tblViewCredits;
+    private TableColumn titleColumn = new TableColumn();
+    private TableColumn titleColumn2 = new TableColumn();
     StageChange stageChange = new StageChange();
 
     public void initialize()
     {
-        title.setText(FrontPageController.getProgram().get(0).toString().substring(6));
+        lblTitle.setText(FrontPageController.getProgram().get(0).toString().substring(7));
+        lblReleaseYear.setText(FrontPageController.getProgram().get(1).toString().substring(14));
+        txtDescription.setText(FrontPageController.getProgram().get(2).toString().substring(13));
+
+        // Column
+        titleColumn.setText("Rolle");
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("rolle"));
+        titleColumn.setMinWidth(10);
+        titleColumn.setPrefWidth(192);
+        titleColumn.setMaxWidth(5000);
+
+        titleColumn2.setText("Navn");
+        titleColumn2.setMinWidth(10);
+        titleColumn2.setPrefWidth(192);
+        titleColumn2.setMaxWidth(5000);
+
+        tblViewCredits.getColumns().addAll(titleColumn,titleColumn2);
+
     }
+
 
     public void handleBackButton(ActionEvent event)
     {
