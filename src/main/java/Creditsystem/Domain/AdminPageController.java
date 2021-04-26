5c4bd8f8d85aedeaec81baa4fb03b3387d6651f0
Mathias Nielsen <mnhnielsen@gmail.com>
2021-04-26 10:10:21 +0200

    Updated admin page
    Placeholder...

--- src/main/java/Creditsystem/Domain/AdminPageController.java
+++ src/main/java/Creditsystem/Domain/AdminPageController.java
@@ -1,6 +1,13 @@
 package Creditsystem.Domain;
 
+import javafx.event.ActionEvent;
+
 public class AdminPageController
 {
+    StageChange stageChange = new StageChange();
 
+    public void handleBackButton(ActionEvent event)
+    {
+        stageChange.handleBackButton(event,"FrontPage.fxml","Krediterings forside");
+    }
 }
