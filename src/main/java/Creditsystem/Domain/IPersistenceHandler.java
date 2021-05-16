b0cd212bc8fa4ff2779224ca16afc2ab82360064
Mathias Nielsen <mnhnielsen@gmail.com>
2021-05-16 10:13:07 +0200

    Done

--- src/main/java/Creditsystem/Domain/IPersistenceHandler.java
+++
@@ -1,5 +0,0 @@
-package Creditsystem.Domain;
-
-public interface IPersistenceHandler
-{
-}
--- src/main/java/Creditsystem/Presentation/App.java
+++ src/main/java/Creditsystem/Presentation/App.java
@@ -1,7 +1,5 @@
 package Creditsystem.Presentation;
 
-import Creditsystem.Data.PersistenceHandler;
-import Creditsystem.Domain.IPersistenceHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
